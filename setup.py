1baea8d12f2009c7e98fcba28111cf3a132ff8de
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2023-06-24 17:17:13 +0700

    Modify 'setup.py'

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name = 'optimal_complete_portfolio',
     version = '0.1',
     description = 'This is a Python project on portfolio optimization.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author = 'TheMetaSetter',
     author_email = 'nguyenanhkhoi0608@gmail.com',
     url = 'https://github.com/TheMetaSetter/optimal-complete-portfolio-python-project',
